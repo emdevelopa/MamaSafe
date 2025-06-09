e4661f7e8f8866598ce6798ca7988a4a2e58ede7
emdevelopa <gyimahsunday@gmail.com>
2025-06-10 00:28:13 +0100

    updated

--- src/pages/appointments.jsx
+++ src/pages/appointments.jsx
@@ -279,7 +279,7 @@ export default function Appointments() {
           </div>
         </div>
 
-        <div className="px-6 py-8">
+        <div className="md:px-6 py-8">
           <div className="flex gap-2 justify-between items-center mb-6">
             <h2 className="md:text-xl font-semibold">My Reminders</h2>
             <button
@@ -387,20 +387,20 @@ export default function Appointments() {
                 className="w-full border px-3 py-2 rounded"
               />
               <input
-                type="text"
-                placeholder="Time"
-                value={formData.time}
-                onChange={(e) =>
-                  setFormData({ ...formData, time: e.target.value })
-                }
-                className="w-full border px-3 py-2 rounded"
-              />
-              <input
-                type="text"
+                type="date"
                 placeholder="Date"
                 value={formData.date}
                 onChange={(e) =>
                   setFormData({ ...formData, date: e.target.value })
+                }
+                className="w-full border px-3 py-2 rounded"
+              />
+              <input
+                type="time"
+                placeholder="Time"
+                value={formData.time}
+                onChange={(e) =>
+                  setFormData({ ...formData, time: e.target.value })
                 }
                 className="w-full border px-3 py-2 rounded"
               />
