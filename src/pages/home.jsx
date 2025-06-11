ebf0dcb780aeb31abbf4482a088172843ab19c13
emdevelopa <gyimahsunday@gmail.com>
2025-06-11 15:33:48 +0100

    updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -95,7 +95,7 @@ export default function Home() {
         </div>
         <img
           className="md:w-[50%] w-[100%] rounded-xl object-cover"
-          src="https://balaoverseas.com/wp-content/uploads/2022/11/bg22.jpg"
+          src="https://images.unsplash.com/photo-1591451855781-9eb0f9726a33?q=80&w=1630&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="dddd"
         />
       </div>
